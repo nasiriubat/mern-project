b7f66d59ce01eeda10a0e5f20abcae23b05d6515
mhnash <nasir.shuvo@tuni.fi>
2024-09-15 23:44:24 +0300

    port into env

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from './routes/product.js';
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
@@ -14,7 +15,7 @@ app.use('/api/products',productRoutes);
 console.log(process.env.MONGO_URI); // express 4 way
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:5000')
+    console.log('Server started at http://localhost:'+PORT);
 });
